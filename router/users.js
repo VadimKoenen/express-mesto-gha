87788d4f0c64f8a0e97ae540f2878d53c596f6ab
Vadim Koenen <lekovad@gmail.com>
2023-08-17 12:29:09 +0300

    14.3

--- router/users.js
+++ router/users.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 
 const {
-  createUser,
   getUsers,
   getUserById,
   updateAvatar,
   updateProfile,
-  login,
   getUserData,
 } = require('../controllers/users');
 
@@ -14,16 +12,13 @@ const {
   getUserByIdValidation,
   updateProfileDataValidation,
   updateAvatarAvatarValidation,
-  userValidation,
-  signinValidation,
 } = require('../middlewares/validation');
 
 router.get('/', getUsers);
 router.get('/:id', getUserByIdValidation, getUserById);
-router.post('/signin', signinValidation, login);
-router.post('/', userValidation, createUser);
+router.get('/me', getUserData);
+
 router.patch('/me', updateProfileDataValidation, updateProfile);
 router.patch('/me/avatar', updateAvatarAvatarValidation, updateAvatar);
-router.get('/me', getUserData);
 
 module.exports = router;
