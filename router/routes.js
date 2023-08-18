cef7b397ed79a674695ea411918c6b11c4a3e27f
Vadim Koenen <lekovad@gmail.com>
2023-08-18 12:42:45 +0300

    14.22

--- router/routes.js
+++ router/routes.js
@@ -11,6 +11,7 @@ const {
   signInValidation,
   userValidation,
 } = require('../middlewares/validation');
+const { NOT_FOUND } = require('../utils/consts');
 
 router.post('/signin', signInValidation, login);
 router.post('/signup', userValidation, createUser);
@@ -19,5 +20,8 @@ router.use(auth);
 
 router.use('/cards', cardsRouter);
 router.use('/users', userRouter);
+router.use('*', (_, res, next) => {
+  next(new NOT_FOUND('Page not found'));
+});
 
 module.exports = router;
